78e3a56161982f2b25c4883d303f60e98bdad028
pradeepkumar28302 <nkpradeepkumar28@gmail.com>
2024-03-30 13:29:01 +0530

    modified the js file for the to-do list app

--- app.js
+++ app.js
@@ -1,25 +1,52 @@
-var list=[];
-let activity=document.getElementById("input");
-let todoAppList=document.getElementById("todolist");
-document.getElementById("button").onclick=activityClickIt;
-function activityClickIt(){
-    list.push(activity.value);
-    console.log(list);
-    activity.value=""
-    show();
-}
-function show(){
-    todoAppList.innerHTML="";
-    list.forEach((n,i)=>{
-        todoAppList.innerHTML+="<li>"+n+"<a onclick='edit("+i+")'>Edit</a>"+"<a onclick='deleteIt("+i+")'>&items|</a></li>";
-    })
-}
-function deleteIt(i){
-    list.splice(i,1);
-    show();
-}
-function edit(i){
-    let value=prompt("Please enter the new task!!!");
-    list.splice(i,1,value);
-    show();
-}
+document.addEventListener("DOMContentLoaded", function() {
+    const list = [];
+    const activityInput = document.getElementById("input");
+    const todoList = document.getElementById("todolist");
+    const addButton = document.getElementById("button");
+  
+    addButton.addEventListener("click", function() {
+      const activity = activityInput.value.trim();
+      if (activity !== "") {
+        list.push(activity);
+        activityInput.value = "";
+        showList();
+      }
+    });
+  
+    function showList() {
+      todoList.innerHTML = "";
+      list.forEach((activity, index) => {
+        const listItem = document.createElement("li");
+        listItem.textContent = activity;
+        
+        const editButton = document.createElement("button");
+        editButton.textContent = "Edit";
+        editButton.addEventListener("click", function() {
+          editActivity(index);
+        });
+        
+        const deleteButton = document.createElement("button");
+        deleteButton.textContent = "Delete";
+        deleteButton.addEventListener("click", function() {
+          deleteActivity(index);
+        });
+        
+        listItem.appendChild(editButton);
+        listItem.appendChild(deleteButton);
+        todoList.appendChild(listItem);
+      });
+    }
+  
+    function deleteActivity(index) {
+      list.splice(index, 1);
+      showList();
+    }
+  
+    function editActivity(index) {
+      const newValue = prompt("Please enter the new task:");
+      if (newValue !== null) {
+        list[index] = newValue.trim();
+        showList();
+      }
+    }
+  });  
