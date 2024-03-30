822105e5892f837458d2e6d0dfb6405aa314520c
pradeepkumar28302 <nkpradeepkumar28@gmail.com>
2024-03-30 13:03:53 +0530

    modified the js file for the to-do list app

--- app.js
+++ app.js
@@ -1,32 +1,25 @@
-var listOfActivities=[]
-
-var input=document.getElementById("input")
-
-var todolist=document.getElementById("todolist")
-
-document.getElementById("button").onclick=click;
-
-function click(){
-    listOfActivities.push(input.value)
-    console.log(listOfActivities)
-    input.value=""
-    showList();
+var list=[];
+let activity=document.getElementById("input");
+let todoAppList=document.getElementById("todolist");
+document.getElementById("button").onclick=activityClickIt;
+function activityClickIt(){
+    list.push(activity.value);
+    console.log(list);
+    activity.value=""
+    show();
 }
-
-function showList(){
-    todolist.innerHTML=""
-    listOfActivities.forEach(function(n,i){
-        todolist.innerHTML+="<li>"+n+"<a onclick='editItem("+i+")'>Edit</a>"+"<a onclick='deleteItem("+i+")'>&items|</a></li>"
+function show(){
+    todoAppList.innerHTML="";
+    list.forEach((n,i)=>{
+        todoAppList.innerHTML+="<li>"+n+"<a onclick='edit("+i+")'>Edit</a>"+"<a onclick='deleteIt("+i+")'>&items|</a></li>";
     })
 }
-
-function deleteItem(i){
-    listOfActivities.splice(i,1)
-    showList()
+function deleteIt(i){
+    list.splice(i,1);
+    show();
 }
-
-function editItem(i){
-    var newValue=prompt("Please insert your new value");
-    listOfActivities.splice(i,1,newValue)
-    showList()
+function edit(i){
+    let value=prompt("Please enter the new task!!!");
+    list.splice(i,1,value);
+    show();
 }
